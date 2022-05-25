6ae8e7865ba1285ce68321ce4109f5d42209db58
Kevin Diaz <kevindi0695@gmail.com>
2022-05-25 14:18:39 -0300

    juego de la vida 0.1

---
+++ src/rows/Row.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react'
+import { typeCellState } from '../config/Config'
+import './Row.css'
+
+export const Row = ({ row, toggleCellStateRow, rowIndex }) => {
+  
+  const [cellState, setCellState] = useState(false);
+
+  useEffect(() => {
+    setCellState(row)
+  }, [row])
+
+  const cellStateRepresentation = cellState === typeCellState?.ALIVE ? "black" : "white";  const handleChangeCellState = () => {
+    setCellState(!cellState);
+    toggleCellStateRow(rowIndex, cellState);
+  };
+
+  return (
+    <div
+      onClick={handleChangeCellState}
+      className={`grid__row grid__row--${cellStateRepresentation}`}
+    >      
+    </div>
+  )
+}
